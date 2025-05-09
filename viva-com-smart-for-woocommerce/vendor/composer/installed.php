90b90e592fff394b2c4953892bb2657f9e2a5292
Sean Murphy <sean@iamseanmurphy.com>
2025-05-09 01:47:59 +0000

    1.0.1

---
+++ viva-com-smart-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vivacom/woocommerce',
+        'pretty_version' => 'dev-59ed2595eb0bf8bd239d3355027895ba6562b1c1',
+        'version' => 'dev-59ed2595eb0bf8bd239d3355027895ba6562b1c1',
+        'reference' => '59ed2595eb0bf8bd239d3355027895ba6562b1c1',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5b390889ecbb17bfa69ed5a030fa2e6075a19ba0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(
+                0 => '2.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'vivacom/woocommerce' => array(
+            'pretty_version' => 'dev-59ed2595eb0bf8bd239d3355027895ba6562b1c1',
+            'version' => 'dev-59ed2595eb0bf8bd239d3355027895ba6562b1c1',
+            'reference' => '59ed2595eb0bf8bd239d3355027895ba6562b1c1',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
